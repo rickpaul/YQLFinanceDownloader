b74aceabe7fca473a6cbf3593b1c2c2fcfb47a32
Rickpaul Dhindsa <rickpaul.dhindsa@gmail.com>
2015-03-03 10:27:45 +1100

    Created Main File
    For running YQL Data Download beginning to end

---
+++ YQLDataDownloader.Main.py
@@ -0,0 +1,20 @@
+import FinStmtDatabaseCreator as finDBMaker
+import FinStmtDataDownloader as finDld
+import FinStmtDatabaseCreator as finDBMaker
+
+def createDatabase():
+	# Create DB
+	if not finDBMaker.checkIfDBExistsWithTables():
+		finDBMaker.doOneTimeDBCreation()
+
+def fillTickerDatabase():
+	# Fill Sectors and Industries
+	finDld.fillSectorAndIndustryDatabase()
+	# Fill Tickers
+	while True:
+		if not finDld.to_DB_fillTickerDatabase(industryUpdateLimit=10, verbose=False):
+			break
+
+if __name__ == '__main__':
+	createDatabase()
+	fillTickerDatabase()
